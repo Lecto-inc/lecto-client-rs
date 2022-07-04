7455ce4ae8ea589ac8cea8dc8399bbb6b14700b7
KatsukiFujimoto <kf1996cnbl@gmail.com>
2022-07-04 11:10:07 +0900

    [CU-2pjagpn] 加盟店に関してはリクエストとレスポンスのStructは同じはずなので、別々のStructを作る必要がないためRequstを名前から外した

--- src/fixture.rs
+++ src/fixture.rs
@@ -1,6 +1,6 @@
 use serde_json::json;
 
-use crate::debt::{DebtRequest, PartnerRequest};
+use crate::debt::{DebtRequest, Partner};
 use crate::debt_status::DebtStatusRequest;
 use crate::debtor::DebtorRequest;
 
@@ -32,7 +32,7 @@ pub fn debt_request_sample_data() -> DebtRequest {
             r#"lease_id:xxxx lease_contract_id:xxxxx item_name:Windowsノートパソコン transaction_id:HGBVPKRN_1LCBU8F requester_name:ヤギ ナツキ total_amount:15240 elapsed_month:-2"#.into(),
         ),
         remind_segments: vec!["y2021".into()],
-        partner: Some(PartnerRequest {
+        partner: Some(Partner {
             id: "1234-5678".into(),
             name: "加盟店アメリケン".into(),
         }),
